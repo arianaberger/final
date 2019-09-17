090f41d3ca7895d74f8de6b88ab7a66d81f52b61
Ariana <ariana.berger@gmail.com>
2019-09-16 21:19:40 -0400

    Fill out create and delete methods in sessions controller

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,6 +1,15 @@
 class SessionController < ApplicationController
 
   def create
+    @user = User.find(email: params[:email])
+
+    if @ user && @authenticate(params[:password])
+      render json: @user
+    else
+      render json: {
+        error: "Invalid credentials"
+      }, status: :unauthorized
+    end
   end
 
   def delete
